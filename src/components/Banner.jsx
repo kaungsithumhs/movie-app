8e8cdd4aae32de2d935dba476bba8a085d80db56
kyawlinnthant <kyawlinnthant.mhs@gmail.com>
2022-11-03 16:20:39 +0630

    button and poster movie detail image fix

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,12 +1,15 @@
-import { useState, useEffect, useMemo } from 'react';
-import { instance, requests, base_url } from '../utils/tmdbApi';
+import { useState, useEffect, useMemo } from "react";
+import { instance, requests, base_url } from "../utils/tmdbApi";
 const Banner = (props) => {
   const [moviesF, setMoviesF] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       const request = await instance.get(requests.fetchBanner);
-      let randomMovie = request.data.results[Math.floor(Math.random() * request.data.results.length - 1)];
+      let randomMovie =
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ];
       setMoviesF(randomMovie);
       return request;
     };
@@ -14,16 +17,16 @@ const Banner = (props) => {
   }, []);
 
   return (
-    <section className=' text-white'>
-      <div className='hero-image  relative'>
+    <section className=" text-white">
+      <div className="hero-image  relative">
         <img
-          className=' h-96  min-w-full object-cover bg-no-repeat object-center-bottom opacity-[0.9]    '
+          className=" h-96  min-w-full object-cover bg-no-repeat object-center-bottom opacity-[0.9]    "
           src={`${base_url}${moviesF.poster_path}`}
           alt={moviesF.name}
         />
-        <div className='absolute bottom-5 px-3  ml-2   '>
-          <p className='text-2xl mb-3'>{moviesF.name}</p>
-          <p className='w-96 text-sm'>{moviesF.overview}</p>
+        <div className="absolute bottom-5 px-3  ml-2   ">
+          <p className="text-2xl mb-3">{moviesF.name}</p>
+          <p className="w-96 text-sm">{moviesF.overview}</p>
         </div>
       </div>
     </section>
--- src/pages/MovieInformationScreen.jsx
+++ src/pages/MovieInformationScreen.jsx
@@ -1,24 +1,32 @@
-import React, { useState } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
-import { getMovieDetail, base_url } from '../utils/tmdbApi';
-import { useQuery } from '@tanstack/react-query';
+import React, { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { getMovieDetail, base_url } from "../utils/tmdbApi";
+import { useQuery } from "@tanstack/react-query";
 
-import Cast from '../components/Cast';
-import PreviewModal from '../components/PreviewModal';
+import Cast from "../components/Cast";
+import PreviewModal from "../components/PreviewModal";
 
 const TestDetail = () => {
   const { id } = useParams();
   const navigate = useNavigate();
   const [backdrop, setBackdrop] = useState(true);
 
-  const { isLoading, isError, error, data: detail } = useQuery(['movieDetail', id], () => getMovieDetail(id));
+  const {
+    isLoading,
+    isError,
+    error,
+    data: detail,
+  } = useQuery(["movieDetail", id], () => getMovieDetail(id));
 
   // if (isLoading) return <p className='h-full w-full absolute inset-0'>Loading...</p>;
-  if (isLoading) return <p className='h-full w-full absolute inset-0'>Loading...</p>;
+  if (isLoading)
+    return <p className="h-full w-full absolute inset-0">Loading...</p>;
 
   const getMovieTrailerVideoKey = (videosList) => {
     if (videosList && videosList.length > 0) {
-      const trailers = videosList.filter(({ type, official }) => type.toLowerCase() === 'trailer' && official);
+      const trailers = videosList.filter(
+        ({ type, official }) => type.toLowerCase() === "trailer" && official
+      );
       return trailers.length > 0 ? trailers.at(-1).key : videosList.at(-1).key;
     }
     return null;
@@ -34,54 +42,77 @@ const TestDetail = () => {
     setBackdrop(!backdrop);
   };
   return (
-    <div className='min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12' key={detail?.data?.id}>
-      <div className='py-3 sm:max-w-4xl sm:mx-auto'>
-        <div className='bg-white shadow-lg border-gray-100 min-h-[500px] border sm:rounded-3xl flex justify-between'>
-          <div className=' overflow-visible w-[45%] p-1 '>
+    <div
+      className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12"
+      key={detail?.data?.id}
+    >
+      <div className="py-3 sm:max-w-4xl sm:mx-auto">
+        <div className="bg-white shadow-lg border-gray-100 min-h-[500px] border sm:rounded-3xl flex justify-between">
+          <div className=" overflow-visible w-[45%] p-1 bg-center">
             <img
-              className='rounded-l-2xl shadow-lg mx-auto h-full object-cover bg-center '
-              src={`${base_url}${detail?.data?.backdrop_path}`}
-              alt=''
+              className="rounded-l-2xl shadow-lg w-full h-full object-cover bg-center"
+              src={`${base_url}${detail?.data?.poster_path}`}
+              alt="movies cover"
             />
           </div>
           <div
-            id='defaultModal'
-            tabindex='-1'
-            aria-hidden='true'
+            id="defaultModal"
+            tabindex="-1"
+            aria-hidden="true"
             className={`${
-              backdrop ? 'hidden' : ' '
+              backdrop ? "hidden" : " "
             } overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full`}
           >
             {/* Modal */}
-            <PreviewModal backdropHandler={backdropHandler} trailerKey={trailerKey} backdrop={backdrop} />
+            <PreviewModal
+              backdropHandler={backdropHandler}
+              trailerKey={trailerKey}
+              backdrop={backdrop}
+            />
           </div>
 
-          <div className='flex flex-col w-[50%] p-6 rounded-md'>
-            <div className='flex items-start'>
-              <h2 className='text-2xl font-bold font-sans '>{detail?.data?.title}</h2>
+          <div className="flex flex-col w-[50%] p-6 rounded-md">
+            <div className="flex items-start">
+              <h2 className="text-2xl font-bold font-sans ">
+                {detail?.data?.title}
+              </h2>
 
-              <div className='bg-yellow-400 font-bold rounded-xl p-2 mx-auto'>
+              <div className="bg-yellow-400 font-bold rounded-xl p-2 mx-auto">
                 {Math.ceil(detail?.data?.vote_average).toFixed(1)}
               </div>
             </div>
 
-            <div className=' text-lg items-center justify-between  '>
-              <p className='mt-2 italic font-bold'>{detail?.data?.release_date.split('-').slice(0, 1)}</p>
+            <div className=" text-lg items-center justify-between  ">
+              <p className="mt-2 italic font-bold">
+                {detail?.data?.release_date.split("-").slice(0, 1)}
+              </p>
               <button
-                className='bg-yellow-500 text-white p-2 mr-6  rounded-md  text-sm hover:bg-yellow-700 hover:duration-200 hover:transition-all my-3'
-                data-modal-toggle='defaultModal'
+                className="bg-yellow-500 text-white p-2 mr-6  rounded-md  text-sm hover:bg-yellow-700 hover:duration-200 hover:transition-all my-3"
+                data-modal-toggle="defaultModal"
                 onClick={backdropHandler}
               >
                 Preview
               </button>
             </div>
 
-            <p className=' text-gray-400 max-h-40 overflow-y-auto'>{detail?.data?.overview}</p>
+            <p className=" text-gray-400 h-[200px] overflow-y-auto">
+              {detail?.data?.overview}
+            </p>
+            <button
+              onClick={() => navigate(-1)}
+              className="absolute bottom-[50px] right-[200px] text-blue-500 hover:text-red-500 cursor-pointer"
+            >
+              &laquo; Go Back
+            </button>
           </div>
         </div>
 
         {/* Cast */}
-        <Cast castData={detail?.data?.credits?.cast} profileHandler={goToProfile} baseUrl={base_url} />
+        <Cast
+          castData={detail?.data?.credits?.cast}
+          profileHandler={goToProfile}
+          baseUrl={base_url}
+        />
       </div>
     </div>
   );
