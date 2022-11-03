88cbae93ee7d21100f5aa51259355e37822090d1
kaungsithumhs <kaungsithu.mhs@gmail.com>
2022-11-03 17:06:24 +0630

    responsive

--- src/components/MoviesCarousel.jsx
+++ src/components/MoviesCarousel.jsx
@@ -6,8 +6,7 @@ import { Navigation, EffectFade } from 'swiper';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/effect-fade';
-import { ImageLoader } from 'render-img';
-import smImg from '../bg.jpg';
+
 import ImageLoading from './ImageLoading';
 const MoviesCarousel = ({ movieData, error, isError, loading, title }) => {
   const navigate = useNavigate();
@@ -25,13 +24,38 @@ const MoviesCarousel = ({ movieData, error, isError, loading, title }) => {
         background: 'linear-gradient(90deg, #b6b6b6ff 0%, #7a7afcff 51%)',
       }}
     >
-      <h1 className='text-sky-900 text-5xl font-mono text-start uppercase mt-8 antialiased ml-5'>{title}</h1>
-      <Swiper spaceBetween={100} slidesPerView={6} modules={[Navigation, EffectFade]}>
+      <h1 className='text-sky-900  text-3xl sm:text-5xl font-mono text-start uppercase mt-8 antialiased ml-5'>
+        {title}
+      </h1>
+      <Swiper
+        spaceBetween={100}
+        slidesPerView={6}
+        modules={[Navigation, EffectFade]}
+        breakpoints={{
+          400: {
+            width: 400,
+            slidesPerView: 1,
+          },
+          500: {
+            width: 500,
+            slidesPerView: 2,
+          },
+          640: {
+            width: 640,
+            slidesPerView: 3,
+          },
+
+          768: {
+            width: 768,
+            slidesPerView: 4,
+          },
+        }}
+      >
         {movieData?.data.results.map((md) => (
           <SwiperSlide key={md.id}>
             <div className='pb-32 mb-14 px-3'>
               <div
-                className='px-2 pb-3 h-32 w-48 relative object-top  rounded-lg z-10  my-5  hover:cursor-pointer hover:scale-90  transition ease-in-out duration-500'
+                className='px-2 pb-3 h-32 w-48 relative object-top  rounded-lg z-10  my-5  hover:cursor-pointer hover:scale-90  transition ease-in-out duration-500 flex flex-wrap'
                 onClick={() => handleClick(md.id)}
               >
                 <ImageLoading baseUrl={base_url} poster={md.poster_path} />
